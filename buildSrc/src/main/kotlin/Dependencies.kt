12234c3c68597930fc0a4646c6a9580d3902818f
Alisa <alicekjob@gmail.com>
2023-12-06 21:42:44 +0300

    Added dependencies module

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,5 @@
+object Dependencies {
+    const val retrofit = "com.squareup.retrofit2:retrofit:2.9.0"
+    const val converter_gson = "com.squareup.retrofit2:converter-gson:2.9.0"
+    const val okhttp = "com.squareup.okhttp3:okhttp:4.10.0"
+}
